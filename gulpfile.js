53be06372cc3d2e73618f91af577a122f3516abf
Steve Gengler <stevegengler@gmail.com>
2015-05-21 00:27:36 -0400

    add sortable table of searches

--- gulpfile.js
+++ gulpfile.js
@@ -8,58 +8,58 @@ var reactify = require('reactify');
 var streamify = require('gulp-streamify');
 
 var path = {
-  HTML: 'src/index.html',
-  MINIFIED_OUT: 'build.min.js',
-  OUT: 'build.js',
-  DEST: 'dist',
-  DEST_BUILD: 'dist/build',
-  DEST_SRC: 'dist/src',
-  ENTRY_POINT: './src/js/App.jsx'
+	HTML: 'src/index.html',
+	MINIFIED_OUT: 'scripts.min.js',
+	OUT: 'scripts.js',
+	DEST: 'dist',
+	DEST_BUILD: 'dist/js',
+	DEST_SRC: 'dist/js',
+	ENTRY_POINT: './src/js/App.jsx'
 };
 
 gulp.task('copy', function(){
-  gulp.src(path.HTML)
-    .pipe(gulp.dest(path.DEST));
+	gulp.src(path.HTML)
+		.pipe(gulp.dest(path.DEST));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(path.HTML, ['copy']);
+	gulp.watch(path.HTML, ['copy']);
 
-  var watcher  = watchify(browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [reactify],
-    debug: true,
-    cache: {}, packageCache: {}, fullPaths: true
-  }));
+	var watcher  = watchify(browserify({
+		entries: [path.ENTRY_POINT],
+		transform: [reactify],
+		debug: true,
+		cache: {}, packageCache: {}, fullPaths: true
+	}));
 
-  return watcher.on('update', function () {
-    watcher.bundle()
-      .pipe(source(path.OUT))
-      .pipe(gulp.dest(path.DEST_SRC))
-      console.log('Updated');
-  })
-    .bundle()
-    .pipe(source(path.OUT))
-    .pipe(gulp.dest(path.DEST_SRC));
+	return watcher.on('update', function () {
+		watcher.bundle()
+			.pipe(source(path.OUT))
+			.pipe(gulp.dest(path.DEST_SRC))
+			console.log("Updated");
+	})
+		.bundle()
+		.pipe(source(path.OUT))
+		.pipe(gulp.dest(path.DEST_SRC));
 });
 
 gulp.task('build', function(){
-  browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [reactify],
-  })
-    .bundle()
-    .pipe(source(path.MINIFIED_OUT))
-    .pipe(streamify(uglify(path.MINIFIED_OUT)))
-    .pipe(gulp.dest(path.DEST_BUILD));
+	browserify({
+		entries: [path.ENTRY_POINT],
+		transform: [reactify],
+	})
+		.bundle()
+		.pipe(source(path.MINIFIED_OUT))
+		.pipe(streamify(uglify(path.MINIFIED_OUT)))
+		.pipe(gulp.dest(path.DEST_BUILD));
 });
 
 gulp.task('replaceHTML', function(){
-  gulp.src(path.HTML)
-    .pipe(htmlreplace({
-      'js': 'build/' + path.MINIFIED_OUT
-    }))
-    .pipe(gulp.dest(path.DEST));
+	gulp.src(path.HTML)
+		.pipe(htmlreplace({
+			'js': 'build/' + path.MINIFIED_OUT
+		}))
+		.pipe(gulp.dest(path.DEST));
 });
 
 gulp.task('production', ['replaceHTML', 'build']);
--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,6 +1,3 @@
-// var Parent = require('./Parent');
-
-// React.render(<Parent />, document.getElementById('app'));
 
 var FriendsContainer = React.createClass({
 	getInitialState: function(){
@@ -11,7 +8,15 @@ var FriendsContainer = React.createClass({
 	},
 	getUser: function(username) {
 		var self = this;
-		$.get('https://api.github.com/users/' + username, function(data) {
+
+		var apiCreds = '?client_id=9d0b0fe0b89a917aaf7c&client_secret=6ad113d9305d7bf752ba07b647a04e1ec49d24f9';
+
+		var params = {
+			client_id: "9d0b0fe0b89a917aaf7c",
+			client_secret: "6ad113d9305d7bf752ba07b647a04e1ec49d24f9"
+		}
+
+		$.get('https://api.github.com/users/' + username, params, function(data) {
 			data.name = data.name ? data.name : data.login;
 			self.state.searches.push(data);
 			self.setState(data);
@@ -57,9 +62,9 @@ var AddFriend = React.createClass({
 	},
 	render: function(){
 		return (
-			<form onSubmit={this.handleAddNew}>
-				<input type="text" value={this.state.searchInput} onChange={this.updateNewFriend} />
-				<button type="submit"> Lookup </button>
+			<form onSubmit={this.handleAddNew} className={"form-inline"}>
+				<input type="text" className={"form-control"} value={this.state.searchInput} onChange={this.updateNewFriend} />
+				<button type="submit" className={"btn btn-primary"}> Lookup </button>
 			</form>
 		);
 	}
@@ -71,22 +76,70 @@ var SearchList = React.createClass({
 			searches: []
 		}
 	},
+	getInitialState: function() {
+		return {
+			toggle: {}
+		}
+	},
 	handleAddNew: function(e){
-		this.props.searchUser(e.target.value);
+		e.preventDefault();
+		var searchName = e.target.value;
+		console.log(e.target);
+		console.log(this.props.searches);
+		var search = $.grep(this.props.searches, function(e){console.log(e.name); return e.name == searchName; });
+		this.props.searchUser(search.login);
 		this.setState({
 			searchInput: ''
 		});
 	},
+	sortList: function(field) {
+		var isNum = !isNaN(this.props.searches[0][field]),
+			toggleState = !this.state.toggle[field];
+
+		this.state.toggle = {}
+		this.state.toggle[field] = toggleState;
+
+		this.props.searches.sort(function(a,b) { 
+			if(a[field] < b[field]) {return -1}
+			if(a[field] > b[field]) {return 1}
+			return 0;
+		});
+
+		if(isNum) {this.props.searches.reverse()}
+		if(!this.state.toggle[field]) {this.props.searches.reverse()}
+
+		console.log(this.state.toggle);
+
+		this.setState(this.props.searches);
+		this.setState(this.state.toggle);
+	},
 	render: function(){
-		var listItems = this.props.searches.map(function(user){
-			return <li> {user.name} </li>;
+		var listItems = this.props.searches.map(function(user, index){
+			return <tr>
+						<td>{index}</td>
+						<td>{user.name}</td>
+						<td>{user.login}</td>
+						<td>{user.public_repos}</td>
+						<td>{user.followers}</td>
+					</tr>;
 		});
 		return (
 			<div>
-				<h3> Friends </h3>
-				<ul>
-					<span onClick={this.handleAddNew}>{listItems}</span>
-				</ul>
+				<h3> Searches </h3>
+				<table className={"table table-striped"}>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th><a href="javascript:;" onClick={this.sortList.bind(this, 'name')}>Name</a></th>
+							<th><a href="javascript:;" onClick={this.sortList.bind(this, 'login')}>Username</a></th>
+							<th><a href="javascript:;" onClick={this.sortList.bind(this, 'public_repos')}>Repos</a></th>
+							<th><a href="javascript:;" onClick={this.sortList.bind(this, 'followers')}>Followers</a></th>
+						</tr>
+					</thead>
+					<tbody>
+						{listItems}
+					</tbody>
+				</table>
 			</div>
 		)
 	}
